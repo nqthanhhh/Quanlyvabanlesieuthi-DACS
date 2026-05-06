6172755f270d0d13c53374e46cb6802d2c75f7e2
nqthanhhh <quocthanh12tet@gmail.com>
2026-05-07 00:52:42 +0700

    Cập nhật csdl

--- backend_nodejs/src/config/db.js
+++ backend_nodejs/src/config/db.js
@@ -1,15 +1,20 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require("mysql2/promise"); // Đảm bảo đã require thư viện
+require("dotenv").config();
 
+// Khởi tạo biến pool
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 4000,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
+// Xuất biến pool ra để server.js sử dụng
 module.exports = pool;
